f78e3eef1596fd245d24fc3b61baa45434201a04
Traci Chan <tlc668@gmail.com>
2020-08-06 16:55:31 -0400

    added computer choice and user choice

--- app.js
+++ app.js
@@ -10,12 +10,40 @@ const game =()=>{
 
         playBtn.addEventListener("click", ()=>{
             introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
         });
     };
+
+ //Play  Match
+ const playMatch = () => {
+     const options = document.querySelectorAll('.options button');
+     const playerHand =document.querySelector(".player-hand");
+     const computerHand = document.querySelector(".computer-hand");
+     //Computer options
+     //randomly generated # between 0, 1, 2 and associate to r,p,s
+     const computerOptions= ['rock', 'paper', 'scissors'];
+
+     //get button clicked
+     options.forEach(option=>{
+         option.addEventListener("click", function(){
+             console.log(this);
+             //computer choice 
+             const computerNumber = Math.floor(Math.random() * 3);
+             const computerChoice = computerOptions[computerNumber];
+             console.log("Computer choice: " + computerChoice);
+         });
+     });
+     
+     
+ } 
 //call the innder functions
 startGame();
+playMatch();
 //updateScore();
 };
 
 //start the game function
 game();
+
+
+
